769e2b4d8e6930cd95a8086eedbc89a4d7eac688
Тернавський Яромир Миколайович <krokocoder123@gmail.com>
2025-08-21 22:35:54 +0300

    Add work function to handle different work types and update balance

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import time
+
 def check_balance(balance):
     print(f"Your balance is {balance} dollars.")
+    return balance
+def work(balance):
+    work_type = input("What type of work do you want to do this time? (full_time, part_time)")
+    time.sleep(1)
+    print("Working...")
+    time.sleep(1)
+
+    if work_type == "full_time":
+        balance += 500
+        print("You worked full time and earned 500 dollars.")
+    elif work_type == "part_time":
+        balance += 250
+        print("You worked part time and earned 250 dollars.")
+    else:
+        print("Invalid work type.")
     return balance
 
 myInventory = []
@@ -17,4 +34,5 @@ actions={
 
 command = input("Select a command: check_balance, work, do_action, exit")
 if command != "exit":
-    
+    if command == "check_balance":
+        startMoney = check_balance(startMoney)
